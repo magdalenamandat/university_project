b457dee70cbd00cd48bc92ef1f8d9cc89455463e
Magdalena <m.mandat@gmail.com>
2018-07-11 16:14:03 +0100

    mentor added

---
+++ src/main/java/models/Mentor.java
@@ -0,0 +1,37 @@
+package models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "mentors")
+public class Mentor {
+
+    private int id;
+    private String name;
+
+    public Mentor(){}
+
+    public Mentor(String name){
+        this.name = name;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    Column(name = "name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
