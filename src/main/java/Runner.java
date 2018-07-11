8cf176c604b4ca56138a154fed324c7db2986102
Magdalena <m.mandat@gmail.com>
2018-07-11 16:10:16 +0100

    runner upddated by new lessons

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,5 +1,6 @@
 import db.DBHelper;
 import models.Course;
+import models.Lesson;
 import models.Student;
 
 public class Runner {
@@ -15,5 +16,13 @@ public class Runner {
         DBHelper.save(course1);
         Course course2 = new Course("Computing", "HNC");
         DBHelper.save(course2);
+
+        Lesson lesson1 = new Lesson("Java", 10);
+        DBHelper.save(lesson1);
+        Lesson lesson2 = new Lesson("Ruby", 20);
+        DBHelper.save(lesson2);
+        Lesson lesson3 = new Lesson("Maya", 25);
+        DBHelper.save(lesson3);
+
     }
 }
